faad9a87f40495a18c4154836bdf06168cf751af
Samuel Heinz <samueljheinz@gmail.com>
2016-08-08 14:55:51 -0700

    bubble_sort now properly returns a sorted array

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,10 +1,16 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
 	if(arr.length <= 1) return arr
 
-	var switch;
+	var switched;
 	do {
-		switch = false;
-		for(var i = 0; )
-	}
-
+		switched = false;
+		for(var i = 0; i < arr.length - 1; i += 1) {
+			if (arr[i] > arr[i + 1]) {
+				var temp_var = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp_var;
+				switched = true;
+			}
+		}
+	} while(switched);
 } 
