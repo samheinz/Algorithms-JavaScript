81b8bb02fa17507821e2badcc68c2c4c20535f0a
Samuel Heinz <samueljheinz@gmail.com>
2016-08-17 16:14:57 -0700

    Algorithm now returns whether or not a string contains all unique characters

--- check_unique.js
+++ check_unique.js
@@ -1,2 +1,14 @@
 function checkUnique(str){
+	if (str.length > 128) return false;
+	
+	var arr = [];
+
+	for(var i = 0; i < str.length; i++){
+		var arr_value = str[i];
+		if(arr[arr_value]) {
+			return false;
+		}
+		arr[arr_value] = true;
+	}
+	return true;
 }
